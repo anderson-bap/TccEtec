04a40af33be65b6a07378fec1f295c3cf052396d
Alberto Frigatto <96782204+Alberto-Frigatto@users.noreply.github.com>
2022-10-27 20:08:54 -0300

    Delete valida directory

--- valida/complemento-endereco.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $complemento=trim($_POST["complemento"]);
-
-    if(empty($complemento))
-        $complemento=null;
-    elseif(preg_match('/[^a-zA-Z0-9,.ÀÁÂÃÇÈÉÊÌÍÎÒÓÔÕÙÚÛÜàáâãäçèéêìíîòóôõùúûü ]/', $complemento))
-        $complemento_err="Preencha o campo somente com letras, espaços e números";
-?>
